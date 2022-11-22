a9e47d77197c995f3d355fe340b18fac7d596f9d
nhphuc236 <huuphucmusic2306@gmail.com>
2022-11-22 16:38:50 +0700

    Nov 2022

---
+++ Bai Tap/W1+W2-InspirationApp/Readme.txt
@@ -0,0 +1,28 @@
+
+	Họ và tên: Nguyễn Hữu Phúc
+	MSSV: 20120161
+	Email: huuphucmusic2306@gmail.com
+
+- Tên solution có chứa "Week01" do bài tuần 2 được cải tiến từ solution của tuần 1, thực chất đây là bài tuần 2 ạ.
+
+- Các chức năng đã hoàn thiện:
+	+ Hiển thị ảnh
+ 	+ Hiển thị câu quotes
+ 	+ Hiển thị nút bấm có chức năng thay đổi quotes và hình ảnh khi được click vào
+		-----Tuần 2-----
+	+ Quản lý các Quote bằng 1 class có 2 attribute: content và imageUri
+	+ Các câu quote và uri của hình ảnh được đặt trong tập tin data.txt
+	+ Đọc hình ảnh bằng đường dẫn tuyệt đối -> phải cài đặt hình ảnh cho button bằng c# thay cho xaml	
+- Những điểm nổ lực làm thêm:
+ 	+ Chỉnh Attribute Resizemode của Window không cho phép User thay đổi kích thước cửa sổ ứng dụng để không bị lỗi (bể ảnh, lệch vị trí chữ so với ảnh,..)
+ 	+ Chỉnh Attribute Windowstartuplocation của Window để khi khởi động ứng dụng nằm ngay giữa màn hình máy tính
+ 	+ Tô màu gradient cho background của Label bằng LinearGradientBrush, điều chỉnh thuộc tính Opacity để thẩm mỹ hơn
+ 	+ Tạo Reflection cho Label
+ 	+ Tạo nút bấm có bằng hình ảnh
+ 	+ Các ảnh được sử dụng như background -> dùng ImageBrush để thay đổi background của Window
+		-----Tuần 2-----
+	+ Tìm hiểu cách đặt hình nền cho button bằng c#
+- Chức năng có thể phát triển thêm:
+	+ Thêm âm nhạc để người dùng thêm hứng thú
+
+- Điểm đề nghị: 10.0
---
+++ Bai Tap/W1+W2-InspirationApp/Source Code/20120161_Week01/InspiringQuotes/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InspiringQuotes
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        class Quote
+        {
+            public string Content { get; set; }
+            public string ImageUri { get; set; }
+
+            public Quote()
+            {
+                Content = "";
+                ImageUri = "";
+            }
+        }
+
+        List<Quote> Quotes = new List<Quote>();
+
+        private void loadData()
+        {
+            string filename = "data.txt";
+            string[] lines = File.ReadAllLines(filename);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] tokens = lines[i].Split(new string[] { " - " }, StringSplitOptions.None);
+
+                string content = tokens[0];
+                string imageuri = tokens[1];
+
+                var quote = new Quote()
+                {
+                    Content = content,
+                    ImageUri = imageuri
+                };
+
+                Quotes.Add(quote);
+            }
+        }
+
+        private void setButtonImage()
+        {
+            string baseUri = AppDomain.CurrentDomain.BaseDirectory;
+            Image img = new Image();
+            img.Source = new BitmapImage(new Uri($"{baseUri}/images/next.png", UriKind.Absolute));
+            img.Height = 60;
+            img.Width = 86;
+            img.RenderTransformOrigin = new Point(0.544, 1.142);
+            img.HorizontalAlignment = HorizontalAlignment.Center;
+            img.VerticalAlignment = VerticalAlignment.Center;
+            img.Stretch = Stretch.Uniform;
+            img.Opacity = 0.7;
+
+            StackPanel stackPnl = new StackPanel();
+            stackPnl.Orientation = Orientation.Horizontal;
+            stackPnl.HorizontalAlignment = HorizontalAlignment.Center;
+            stackPnl.VerticalAlignment = VerticalAlignment.Center;
+            stackPnl.Children.Add(img);
+
+            nextQuoteButton.Content = stackPnl;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            loadData();
+            setButtonImage();
+            createQuote();
+        }
+
+        private void createQuote()
+        { 
+            int i = new Random().Next(0, Quotes.Count());
+
+            quotesLabel.Content = string.Format(Quotes[i].Content,Environment.NewLine);
+
+            // backGroundPicture.Source = new BitmapImage(new Uri(_images[i], UriKind.Relative));
+            //this.Background = new ImageBrush(new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), Quotes[i].ImageUri)));
+            
+            string baseUri = AppDomain.CurrentDomain.BaseDirectory;
+            string imageUri = Quotes[i].ImageUri;
+            string uri = $"{baseUri}{imageUri}";
+            this.Background = new ImageBrush(new BitmapImage(new Uri(uri, UriKind.Absolute)));
+        }
+
+        private void nextQuoteButton_Click(object sender, RoutedEventArgs e)
+        {
+            createQuote();
+        }
+
+
+    }
+}
